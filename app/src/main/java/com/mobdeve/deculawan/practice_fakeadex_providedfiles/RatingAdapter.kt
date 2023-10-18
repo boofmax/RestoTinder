db0b390e169f41db96418fd58bf364bfadbc7da1
Samuel Jedidiah Uy <samueluy@Samuels-Laptop.local>
2023-10-18 10:19:04 +0800

    fixed rating recycler view

--- app/src/main/java/com/mobdeve/deculawan/practice_fakeadex_providedfiles/RatingAdapter.kt
+++ app/src/main/java/com/mobdeve/deculawan/practice_fakeadex_providedfiles/RatingAdapter.kt
@@ -7,7 +7,8 @@ import androidx.recyclerview.widget.RecyclerView
 class RatingAdapter(private val data: ArrayList<RatingModel>) : RecyclerView.Adapter<RatingViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RatingViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_layout, parent, false)
+        val view = inflater.inflate(R.layout.rating_layout, parent, false)
+
 
         return RatingViewHolder(view)
     }
